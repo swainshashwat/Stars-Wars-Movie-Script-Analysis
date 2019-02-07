04e8919227147bbf094a012d8df3e850281122a5
Shashwat Swain <shashwatswainofficial@gmail.com>
2019-02-07 19:57:34 +0530

    reshaped data folder; added wordcloud

--- main.R
+++ main.R
@@ -31,13 +31,38 @@ cleanCorpus <- function(corpus){
 frequentTerms <- function(text){
   s.cor <- Corpus(VectorSource(text))
   s.cor.cl <- cleanCorpus(s.cor)
-  s.tdm <- TermDocumentMatrix(s.tdm, 0.999)
+  s.tdm <- TermDocumentMatrix(s.cor.cl)
+  s.tdm <- removeSparseTerms(s.tdm, 0.999)
   m <- as.matrix(s.tdm)
   word_freqs <- sort(rowSums(m), decreasing=TRUE)
-  dm
+  dm <- data.frame(word=names(word_freqs), freq=word_freqs)
+  return(dm)
 }
 
+# Number of dialogues in episode 4
+print("No. of dialogues in ep4")
+length(ep4$character)
+
+# Number of dialogues in episode 5
+print("No. of dialogues in ep5")
+length(ep5$character)
+
+# Number of dialogues in episode 6
+print("No. of dialogues in ep6")
+length(ep6$character)
 
 
+# Top 20 chars with maximum dialogues
+top.ep4.chars <- as.data.frame(sort(table(ep4$character), decreasing = TRUE))[1:20,]
 
+# Visualization
+ggplot(data=top.ep4.chars, aes(x=Var1, y=Freq)) +
+  geom_bar(stat="identity", fill="blue", colour="black") +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=45, hjust=1)) +
+  labs(x="Character", y="Number of dialogues")
+
+# Wordcloud for Episode IV
+wordcloud2(frequentTerms(ep4$dialogue), size=0.5,
+           figPath = "dataset/wordcloud_masks/vader.png")
 
